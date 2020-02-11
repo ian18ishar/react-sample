593ebefea9a37da9d1faddae94fe18143a013436
ian18ishar <ishar@alterra.id>
2020-02-11 10:32:25 +0700

    new configuration

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Demo Belajar ReactJS v2
+            Demo Belajar ReactJS New Configuration
           </a>
         </header>
       </div>
